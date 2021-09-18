f3255e47a6178acaeb77d7040205e1facde376e8
blueju <blueju@foxmail.com>
2021-09-19 00:18:33 +0800

    temp push

--- docs/components/MoreButton 更多按钮/demo-1.jsx
+++ docs/components/MoreButton 更多按钮/demo-1.jsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import { MyButton, MySpace } from 'blued';
 import { MoreButton } from 'blued-pro';
 
-export default () => <MoreButton title="First Demo" />;
+export default () => {
+  function handleClick(e) {
+    // console.log(e.target.innerHTML);
+    console.log(e);
+  }
+
+  return (
+    <>
+      {/*<Slider value={size} onChange={(value) => setSize(value)} />*/}
+      <MoreButton spaceSize={8} showNumber={3}>
+        {/*<MySpace size={8}>*/}
+        <MyButton onClick={handleClick}>新增</MyButton>
+        <MyButton onClick={handleClick}>修改</MyButton>
+        <MyButton onClick={handleClick}>删除</MyButton>
+        <MyButton onClick={handleClick}>全选</MyButton>
+        <MyButton onClick={handleClick}>查看</MyButton>
+        <MyButton onClick={handleClick}>导出</MyButton>
+        <MyButton onClick={handleClick}>导入</MyButton>
+        <MyButton onClick={handleClick}>打印</MyButton>
+        {/*</MySpace>*/}
+      </MoreButton>
+    </>
+  );
+};
--- src/MoreButton/index.tsx
+++ src/MoreButton/index.tsx
@@ -1,3 +1,89 @@
 import React from 'react';
+// @ts-ignore
+import { MyButton, MyDropdown, MyMenu, MySpace } from 'blued';
+import { DownOutlined } from '@ant-design/icons';
 
-export default ({ title }: { title: string }) => <h1>{title}</h1>;
+interface MoreButtonProps {
+  showNumber: number;
+  spaceSize?: number;
+  children?: React.ReactNode;
+}
+
+const InternalMoreButton: React.ForwardRefRenderFunction<
+  unknown,
+  MoreButtonProps
+> = (props, ref) => {
+  const { showNumber = 5, spaceSize = 8, children, ...rest } = props;
+  console.log('showNumber', showNumber);
+
+  function renderVisibleButton(props: MoreButtonProps) {
+    const { showNumber } = props;
+    return React.Children.map(props.children, (child, index) => {
+      if (index < showNumber) {
+        return child;
+      }
+    });
+  }
+
+  function renderInvisibleButton(props: MoreButtonProps) {
+    const { showNumber } = props;
+    return React.Children.map(props.children, (child, index) => {
+      if (index >= showNumber && React.isValidElement(child)) {
+        return React.cloneElement(child, {
+          style: {
+            display: 'none',
+          },
+        });
+      }
+    });
+  }
+
+  function renderMoreButton(props: MoreButtonProps) {
+    function getDropdownMenu() {
+      return (
+        <MyMenu>
+          {React.Children.map<unknown, React.ReactNode>(
+            props.children,
+            (child, index) => {
+              console.log(child);
+              if (index >= showNumber && React.isValidElement(child)) {
+                return (
+                  <MyMenu.Item key={index} onClick={child.props.onClick}>
+                    {child.props.children}
+                  </MyMenu.Item>
+                );
+              }
+            },
+          )}
+        </MyMenu>
+      );
+    }
+
+    const { showNumber } = props;
+    return (
+      <MyDropdown overlay={getDropdownMenu()}>
+        <MyButton>
+          更多操作 <DownOutlined />
+        </MyButton>
+      </MyDropdown>
+    );
+  }
+
+  return (
+    <>
+      <MySpace size={spaceSize}>
+        {renderVisibleButton(props)}
+        {renderMoreButton(props)}
+      </MySpace>
+      {renderInvisibleButton(props)}
+    </>
+  );
+};
+
+const MoreButton = React.forwardRef<unknown, MoreButtonProps>(
+  InternalMoreButton,
+);
+
+MoreButton.displayName = 'MoreButton';
+
+export default MoreButton;
