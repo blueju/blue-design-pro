b1e136c3630054a27ba1ca72cb53baa18ff10670
blueju <blueju@foxmail.com>
2021-09-24 13:56:59 +0800

    add 更多按钮 demo

--- docs/components/MoreButton 更多按钮/demo-1.jsx
+++ docs/components/MoreButton 更多按钮/demo-1.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { MyButton, MySpace } from 'blued';
+import React, { useState } from 'react';
+import { MyButton, MySlider } from 'blued';
 import { MoreButton } from 'blued-pro';
 
 export default () => {
+  const [sliderSize, setSliderSize] = useState(4);
+
   function handleClick(e) {
-    // console.log(e.target.innerHTML);
     console.log(e);
   }
 
   return (
     <>
-      {/*<Slider value={size} onChange={(value) => setSize(value)} />*/}
-      <MoreButton spaceSize={8} showNumber={3}>
-        {/*<MySpace size={8}>*/}
+      <MySlider
+        value={sliderSize}
+        max={8}
+        onChange={(value) => setSliderSize(value)}
+      />
+      <br />
+      <MoreButton spaceSize={8} showNumber={sliderSize}>
         <MyButton onClick={handleClick}>新增</MyButton>
-        <MyButton onClick={handleClick}>修改</MyButton>
         <MyButton onClick={handleClick}>删除</MyButton>
         <MyButton onClick={handleClick}>全选</MyButton>
         <MyButton onClick={handleClick}>查看</MyButton>
         <MyButton onClick={handleClick}>导出</MyButton>
         <MyButton onClick={handleClick}>导入</MyButton>
         <MyButton onClick={handleClick}>打印</MyButton>
-        {/*</MySpace>*/}
       </MoreButton>
     </>
   );
