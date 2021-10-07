de2c05c4a75acc577c2e434a1aa664257bc0018b
blueju <blueju@foxmail.com>
2021-10-07 17:51:07 +0800

    修改路由模式和静态资源路径，用于推送后构建部署 demo 页面

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'blued-pro',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
+  history: {
+    type: 'hash',
+  },
+  /**
+   * https://d.umijs.org/zh-CN/guide/faq#部署到github-pages
+   */
+  publicPath: './',
 });
